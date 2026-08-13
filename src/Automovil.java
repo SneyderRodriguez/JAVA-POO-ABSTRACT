afc7c05d4a6b0dcfb328db15d0ad2c6ee02cc1a4
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-08-13 12:44:02 -0500

    Interfaces

--- src/Automovil.java
+++ src/Automovil.java
@@ -1,13 +1,25 @@
-public class Automovil extends Vehiculo {
+public class Automovil extends Vehiculo implements Asegurable {
     private double precioGasolina;
+    private double valorComercial;
 
-    public Automovil(String marca, int anio, double precioGasolina) {
+    public Automovil(String marca, int anio, double precioGasolina, double valorComercial) {
         super(marca, anio);
         this.precioGasolina = precioGasolina;
+        this.valorComercial = valorComercial;
     }
 
     @Override
     public double calcularCostoKm() {
         return precioGasolina / 12.0;
+    }
+
+    @Override
+    public double calcularPrima() {
+        return valorComercial * 0.03;
+    }
+
+    @Override
+    public String numeroPoliza() {
+        return "POL-AUTO-" + marca.toUpperCase() + "-" + anio;
     }
 }
