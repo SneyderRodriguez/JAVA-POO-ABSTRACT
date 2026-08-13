ae2dd69b7c9b7e813ef26649a393ec84668fc4f2
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-08-13 14:38:43 -0500

    Interfaces

--- src/Asegurable.java
+++ src/Asegurable.java
@@ -1,4 +1,8 @@
 public interface Asegurable {
     double calcularPrima();
     String numeroPoliza();
+
+    default void registrarPoliza() {
+        System.out.println("Poliza " + numeroPoliza() + " registrada. Prima: $" + calcularPrima());
+    }
 }
